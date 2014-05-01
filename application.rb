274b012dd87580ae4e35dcb3cf3eaa1bcb1a1232
Lars Henrik Mai <lars.mai@kontinui.de>
2014-05-01 15:58:51 +0200

    extract requires into seperate file

---
+++ application.rb
@@ -0,0 +1,11 @@
+#$LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'models'))
+$LOAD_PATH.unshift(File.dirname(__FILE__))
+
+require 'rubygems'
+require 'bundler/setup'
+
+require 'pupa'
+require 'nokogiri'
+
+require 'models/resolution'
+
--- resolution.rb
+++ resolution.rb
@@ -1,13 +1,4 @@
-#$LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'models'))
-$LOAD_PATH.unshift(File.dirname(__FILE__))
-
-require 'rubygems'
-require 'bundler/setup'
-
-require 'pupa'
-require 'nokogiri'
-
-require 'models/resolution'
+require './application'
 
 class ResolutionProcessor < Pupa::Processor
 
