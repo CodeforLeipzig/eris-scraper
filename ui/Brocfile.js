c4da44c16022078a5c880cc8caa71580b49ba9b6
Andreas Haller <andreas.haller@injixo.com>
2014-11-01 10:27:24 +0100

    Add and load foundation.css

--- ui/Brocfile.js
+++ ui/Brocfile.js
@@ -17,5 +17,6 @@ var app = new EmberApp();
 // please specify an object with the list of modules as keys
 // along with the exports of each module as its value.
 app.import('bower_components/elasticsearch/elasticsearch.js')
+app.import('bower_components/foundation/css/foundation.css')
 
 module.exports = app.toTree();
